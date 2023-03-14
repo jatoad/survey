38f9d6ad1af584389536d7691326f0d1d6675d7f
jatoad <jasperhmca.todd@gmail.com>
2023-03-14 15:11:31 +0000

    retrieved data from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +13,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('survey')
 
-questions = SHEET.worksheet('questions')
 
-data = questions.get_all_values()
 
-print(data)
+def survey_structure():
+    """
+    survey overall structure
+    prints instructions of the survey 
+    """
+    print("please fill out all questons in survey")
+    print("provide answers in a scale of 1-10, where 5 is the average")
+    print("example: 7 \n")
+
+def get_questions():
+    question = SHEET.worksheet("questions").get_all_values()
+    for col in question:
+        pprint(question)
+    answer = input("enter answer here:")
+
+
+survey_structure()
+get_questions()    
